652e88cfe8096ed34c193b0f86d58f7fd6b632c6
Srikar Yekollu <srikary@users.noreply.github.com>
2025-01-16 00:21:16 +0400

    Update air_quality_sensor_one.ino
    Working code.
    TODO:
    1. TFT screen format
    2. Red alert font

--- air_quality_sensor_one.ino
+++ air_quality_sensor_one.ino
@@ -13,6 +13,7 @@
 #include <AGS10.h>
 #include <Wire.h>
 #include <PMS.h>
+#include <MQUnifiedsensor.h>
 
 // Connections
 // PMS5003 PM2.5 Sensor
@@ -59,8 +60,6 @@ DFRobot_DHT11 DHT;
 #define TFT_RST        8 // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC         9
 
-//Include the library
-#include <MQUnifiedsensor.h>
 #define         MQ2_PIN                 (A1)
 #define         MQ7_PIN                 (A2)
 #define         Voltage_Resolution      (5)
@@ -68,8 +67,8 @@ DFRobot_DHT11 DHT;
 #define         RatioMQ2CleanAir        (9.83) //RS / R0 = 9.83 ppm 
 #define RatioMQ7CleanAir 27.5 //RS / R0 = 27.5 ppm 
 
-// MQUnifiedsensor MQ2("Arduino Nano", Voltage_Resolution, ADC_Bit_Resolution, MQ2_PIN, "MQ-2");
-// MQUnifiedsensor MQ7("Arduino Nano", Voltage_Resolution, ADC_Bit_Resolution, MQ7_PIN, "MQ-7");
+MQUnifiedsensor MQ2("Arduino", Voltage_Resolution, ADC_Bit_Resolution, MQ2_PIN, "MQ-2");
+MQUnifiedsensor MQ7("Arduino", Voltage_Resolution, ADC_Bit_Resolution, MQ7_PIN, "MQ-7");
 
 unsigned long oldTime = 0;
 
@@ -93,7 +92,7 @@ void setup() {
   Serial.begin(9600);
   pms5003_serial.begin(9600);
   pms_5003_sensor.passiveMode();    // Switch to passive mode
-  Serial.println("Waking up, wait 30 seconds for stable readings");
+  Serial.println(F("Waking up, wait 30 seconds for stable readings"));
   pms_5003_sensor.wakeUp();
   mhz19_serial.begin(9600);                               // (Uno example) device to MH-Z19 serial start   
   mhz19b.begin(mhz19_serial);                                // *Serial(Stream) refence must be passed to library begin(). 
@@ -104,63 +103,63 @@ void setup() {
   ags10_sensor.begin();
   
 
-  // MQ2.setRegressionMethod(1); //_PPM =  a*ratio^b
-  // MQ2.setA(574.25); MQ2.setB(-2.222); // Configure the equation to to calculate LPG concentration
-  // /*
-  //   Exponential regression:
-  //   Gas    | a      | b
-  //   H2     | 987.99 | -2.162
-  //   LPG    | 574.25 | -2.222
-  //   CO     | 36974  | -3.109
-  //   Alcohol| 3616.1 | -2.675
-  //   Propane| 658.71 | -2.168
-  // */
+  MQ2.setRegressionMethod(1); //_PPM =  a*ratio^b
+  MQ2.setA(574.25); MQ2.setB(-2.222); // Configure the equation to to calculate LPG concentration
+  /*
+    Exponential regression:
+    Gas    | a      | b
+    H2     | 987.99 | -2.162
+    LPG    | 574.25 | -2.222
+    CO     | 36974  | -3.109
+    Alcohol| 3616.1 | -2.675
+    Propane| 658.71 | -2.168
+  */
 
   // /*****************************  MQ Init ********************************************/ 
-  // //Remarks: Configure the pin of arduino as input.
-  // /************************************************************************************/ 
-  // MQ2.init(); 
-  // Serial.print("Calibrating MQ2 please wait. Only execute this in Clean Air Conditions");
-  // float calcR0 = 0;
-  // for(int i = 1; i<=10; i ++)
-  // {
-  //   MQ2.update(); // Update data, the arduino will read the voltage from the analog pin
-  //   calcR0 += MQ2.calibrate(RatioMQ2CleanAir);
-  // }
-  // MQ2.setR0(calcR0/10);
-  // Serial.println("Done Calibrating MQ2");
+  //Remarks: Configure the pin of arduino as input.
+  /************************************************************************************/ 
+  MQ2.init(); 
+  Serial.print(F("Calibrating MQ2 please wait. Only execute this in Clean Air Conditions"));
+  float calcR0 = 0;
+  for(int i = 1; i<=10; i ++)
+  {
+    MQ2.update(); // Update data, the arduino will read the voltage from the analog pin
+    calcR0 += MQ2.calibrate(RatioMQ2CleanAir);
+  }
+  MQ2.setR0(calcR0/10);
+  Serial.println(F("Done Calibrating MQ2"));
   
-  // if(isinf(calcR0)) {Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply"); while(1);}
-  // if(calcR0 == 0){Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply"); while(1);}
+  if(isinf(calcR0)) {Serial.println(F("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply")); while(1);}
+  if(calcR0 == 0){Serial.println(F("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply")); while(1);}
   /*****************************  MQ CAlibration ********************************************/ 
 
-  // MQ7.setRegressionMethod(1); //_PPM =  a*ratio^b
-  // MQ7.setA(99.042); MQ7.setB(-1.518); // Configure the equation to calculate CO concentration value
+  MQ7.setRegressionMethod(1); //_PPM =  a*ratio^b
+  MQ7.setA(99.042); MQ7.setB(-1.518); // Configure the equation to calculate CO concentration value
 
-  // /*
-  //   Exponential regression:
-  // GAS     | a      | b
-  // H2      | 69.014  | -1.374
-  // LPG     | 700000000 | -7.703
-  // CH4     | 60000000000000 | -10.54
-  // CO      | 99.042 | -1.518
-  // Alcohol | 40000000000000000 | -12.35
-  // */
+  /*
+    Exponential regression:
+  GAS     | a      | b
+  H2      | 69.014  | -1.374
+  LPG     | 700000000 | -7.703
+  CH4     | 60000000000000 | -10.54
+  CO      | 99.042 | -1.518
+  Alcohol | 40000000000000000 | -12.35
+  */
   
-  // MQ7.init(); 
+  MQ7.init(); 
 
-  // Serial.print("Calibrating MQ7. Please wait");
-  // calcR0 = 0;
-  // for(int i = 1; i<=10; i ++)
-  // {
-  //   MQ7.update(); // Update data, the arduino will read the voltage from the analog pin
-  //   calcR0 += MQ7.calibrate(RatioMQ7CleanAir);
-  // }
-  // MQ7.setR0(calcR0/10);
-  // Serial.println("Done Calibrating MQ7");
+  Serial.print(F("Calibrating MQ7. Please wait"));
+  calcR0 = 0;
+  for(int i = 1; i<=10; i ++)
+  {
+    MQ7.update(); // Update data, the arduino will read the voltage from the analog pin
+    calcR0 += MQ7.calibrate(RatioMQ7CleanAir);
+  }
+  MQ7.setR0(calcR0/10);
+  Serial.println(F("Done Calibrating MQ7"));
   
-  // if(isinf(calcR0)) {Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply"); while(1);}
-  // if(calcR0 == 0){Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply"); while(1);}
+  if(isinf(calcR0)) {Serial.println(F("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply")); while(1);}
+  if(calcR0 == 0){Serial.println(F("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply")); while(1);}
 
 
   tft.init(240, 320);   
@@ -168,20 +167,20 @@ void setup() {
  
      
 void loop() {
-  Serial.println("Starting loop");
+  Serial.println(F("Starting loop"));
   pms5003_serial.listen();
   pms_5003_sensor.requestRead();
   if (pms_5003_sensor.readUntil(pms_5003_data, 10000)) {
     // reading data was successful!
-    Serial.print("PM 1.0 (ug/m3): ");
+    Serial.print(F("PM 1.0 (ug/m3): "));
     Serial.println(pms_5003_data.PM_AE_UG_1_0);
 
-    Serial.print("PM 2.5 (ug/m3): ");
+    Serial.print(F("PM 2.5 (ug/m3): "));
     Serial.println(pms_5003_data.PM_AE_UG_2_5);
 
-    Serial.print("PM 10.0 (ug/m3): ");
+    Serial.print(F("PM 10.0 (ug/m3): "));
     Serial.println(pms_5003_data.PM_AE_UG_10_0);
-    Serial.println("---------------------------------------");
+    Serial.println(F("---------------------------------------"));
   }
 
   mhz19_serial.listen();
@@ -194,12 +193,12 @@ void loop() {
 
       CO2 = mhz19b.getCO2(false, true);                             // Request CO2 (as ppm)
       
-      Serial.print("CO2 (ppm): ");                      
+      Serial.print(F("CO2 (ppm): "));                      
       Serial.println(CO2);                                
 
       
       Temp = mhz19b.getTemperature(true);                     // Request Temperature (as Celsius)
-      Serial.print("Temperature [from MHZ19B] (C): ");                  
+      Serial.print(F("Temperature [from MHZ19B] (C): "));                  
       Serial.println(Temp);                               
 
       mhz19b_co2_data_timer = millis();
@@ -209,49 +208,49 @@ void loop() {
   wz_ze08_sensor.requestRead();
   if (wz_ze08_sensor.readUntil(hcho_data))
   {
-      Serial.println("Sending command");
-      Serial.print("HCHO (ppd): ");
+      Serial.println(F("Sending command"));
+      Serial.print(F("HCHO (ppd): "));
       Serial.println(hcho_data.HCHO_PPB);
-      Serial.print("HCHO (ug/m3): ");
+      Serial.print(F("HCHO (ug/m3): "));
       Serial.println(hcho_data.HCHO_UGM3); 
   }
 
   // if (wz_ze08_sensor.read(hcho_data))
   // {
-  //     Serial.print("HCHO (ppd): ");
+  //     Serial.print(F("HCHO (ppd): "));
   //     Serial.println(hcho_data.HCHO_PPB);
-  //     Serial.print("HCHO (ug/m3): ");
+  //     Serial.print(F("HCHO (ug/m3): "));
   //     Serial.println(hcho_data.HCHO_UGM3); // no data here, 0 returned
   //     Serial.println();
   // } else {
-  //   Serial.println("NO HCHO data");
+  //   Serial.println(F("NO HCHO data"));
   // }
 
   DHT.read(DHT11_PIN);
-  Serial.print("temp:");
+  Serial.print(F("temp:"));
   Serial.print(DHT.temperature);
-  Serial.print("  humi:");
+  Serial.print(F("  humi:"));
   Serial.println(DHT.humidity);
 
-  Serial.print("TVOC ");
+  Serial.print(F("TVOC "));
   Serial.print(ags10_sensor.readTVOC());
-  Serial.println(" ppm");
+  Serial.println(F(" ppm"));
 
 
-  // MQ2.update(); // Update data, the arduino will read the voltage from the analog pin
-  // int lpg_percentage = MQ2.readSensor();
-  // Serial.print("LPG: ");
-  // Serial.print(lpg_percentage);
-  // Serial.println("ppm"); 
+  MQ2.update(); // Update data, the arduino will read the voltage from the analog pin
+  float lpg_percentage = MQ2.readSensor();
+  Serial.print(F("LPG: "));
+  Serial.print(lpg_percentage);
+  Serial.println(F("ppm")); 
   
-  // MQ7.update(); // Update data, the arduino will read the voltage from the analog pin 
-  // int co_percentage = MQ7.readSensor();
-  // Serial.print("CO: ");
-  // Serial.print(co_percentage);
-  // Serial.println("ppm");
+  MQ7.update(); // Update data, the arduino will read the voltage from the analog pin 
+  float co_percentage = MQ7.readSensor();
+  Serial.print(F("CO: "));
+  Serial.print(co_percentage);
+  Serial.println(F("ppm"));
   
 
-  Serial.println("Printing to TFT");
+  Serial.println(F("Printing to TFT"));
   tft.fillScreen(ST77XX_BLACK);
   tft.setCursor(0, 0);
   tft.setTextWrap(false);
@@ -265,33 +264,33 @@ void loop() {
   // tft.setTextColor(ST77XX_MAGENTA);
   tft.setTextSize(3);
 
-  tft.print("PM 1.0: ");
+  tft.print(F("PM 1.0: "));
   tft.println(pms_5003_data.PM_AE_UG_1_0);
-  tft.print("PM 2.5: ");
+  tft.print(F("PM 2.5: "));
   tft.println(pms_5003_data.PM_AE_UG_2_5);
-  tft.print("PM  10: ");
+  tft.print(F("PM  10: "));
   tft.println(pms_5003_data.PM_AE_UG_10_0);
-  tft.print("CO2   : ");
+  tft.print(F("CO2   : "));
   tft.println(CO2);
-  tft.print("Temp  : ");
+  tft.print(F("Temp  : "));
   tft.print(DHT.temperature);
-  tft.println(" C");
-  tft.print("Hum   : "); 
+  tft.println(F(" C"));
+  tft.print(F("Hum   : ")); 
   tft.print(DHT.humidity);
-  tft.println(" %");
-  tft.print("TVOC  :");
+  tft.println(F(" %"));
+  tft.print(F("TVOC  :"));
   tft.print(ags10_sensor.readTVOC());
-  tft.println("ppd");
-  tft.print("HCHO  :");
+  tft.println(F("ppd"));
+  tft.print(F("HCHO  :"));
   tft.print(hcho_data.HCHO_PPB);
-  tft.println("ppd");
-  tft.print("HCHO  :");
+  tft.println(F("ppd"));
+  tft.print(F("HCHO  :"));
   tft.print(hcho_data.HCHO_UGM3);
-  tft.println("g/m3");
+  tft.println(F("g/m3"));
   
-  // tft.print("LPG   : ");
-  // tft.println(lpg_percentage);
-  // tft.print("CO     : ");
-  // tft.println(co_percentage);
+  tft.print(F("LPG   :"));
+  tft.println(lpg_percentage);
+  tft.print(F("CO    :"));
+  tft.println(co_percentage);
   delay(1000);
 }
